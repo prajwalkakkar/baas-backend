38029fcd5fab5946b95a5bbb3e6a1a19cb176ede
prajwalkakkar <prajwal@ixiono.com>
2022-05-27 13:36:40 +0530

    fix cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", userRouter);
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 const app = require("./app");
-const cors = require("cors");
 const port = process.env.PORT || 3000;
-
-app.use(cors());
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
