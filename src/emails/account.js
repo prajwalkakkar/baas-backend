85c8f63eea97be870c1fa53f1b2d9a298786c955
prajwalkakkar <prajwal@ixiono.com>
2022-06-16 16:12:11 +0530

    update: email to mailgun

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,27 +1,34 @@
 require("dotenv").config();
-const sgMail = require("@sendgrid/mail");
+const mailgun = require("mailgun-js");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const mg = mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN,
+});
 
 const sendWelcomeEmail = (email, OTP) => {
- sgMail.send({
-  to: email,
-  from: "vanshukakkar3@gmail.com",
-  subject: "Verification OTP",
-  text: `Welcome to BasS. Your verification code is ${OTP}.`,
- });
+  const data = {
+    from: `No-Reply <${process.env.MAILGUN_EMAIL}>`,
+    to: email,
+    subject: "Welcome to BAAS",
+    text: `Welcome to BaaS. Use code ${OTP} to verify your email`,
+  };
+
+  mg.messages().send(data, (error, body) => {
+    console.log("Message sent!\nBody =>", body);
+  });
 };
 
 const sendCancelationEmail = (email, name) => {
- sgMail.send({
-  to: email,
-  from: "andrew@mead.io",
-  subject: "Sorry to see you go!",
-  text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
- });
+  sgMail.send({
+    to: email,
+    from: "andrew@mead.io",
+    subject: "Sorry to see you go!",
+    text: `Goodbye, ${name}. I hope to see you back sometime soon.`,
+  });
 };
 
 module.exports = {
- sendWelcomeEmail,
- sendCancelationEmail,
+  sendWelcomeEmail,
+  sendCancelationEmail,
 };
